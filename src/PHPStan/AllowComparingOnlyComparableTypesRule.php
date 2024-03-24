35ce43b4675f261f1ad84c2466d34d311963fe89
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-03-24 17:36:13 +0100

    first-commit

---
+++ src/PHPStan/AllowComparingOnlyComparableTypesRule.php
@@ -0,0 +1,161 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nektria\PHPStan;
+
+use DateTimeInterface;
+use PhpParser\Node;
+use PhpParser\Node\Expr\BinaryOp;
+use PhpParser\Node\Expr\BinaryOp\Equal;
+use PhpParser\Node\Expr\BinaryOp\Greater;
+use PhpParser\Node\Expr\BinaryOp\GreaterOrEqual;
+use PhpParser\Node\Expr\BinaryOp\Identical;
+use PhpParser\Node\Expr\BinaryOp\NotEqual;
+use PhpParser\Node\Expr\BinaryOp\NotIdentical;
+use PhpParser\Node\Expr\BinaryOp\Smaller;
+use PhpParser\Node\Expr\BinaryOp\SmallerOrEqual;
+use PhpParser\Node\Expr\BinaryOp\Spaceship;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\Type\FloatType;
+use PHPStan\Type\IntegerType;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\StringType;
+use PHPStan\Type\Type;
+use PHPStan\Type\UnionType;
+use PHPStan\Type\VerbosityLevel;
+
+/**
+ * @implements Rule<BinaryOp>
+ */
+class AllowComparingOnlyComparableTypesRule implements Rule
+{
+    public function getNodeType(): string
+    {
+        return BinaryOp::class;
+    }
+
+    /**
+     * @param BinaryOp $node
+     * @return string[]
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        if (
+            !$node instanceof Greater
+            && !$node instanceof GreaterOrEqual
+            && !$node instanceof Smaller
+            && !$node instanceof SmallerOrEqual
+            && !$node instanceof Spaceship
+            && !$node instanceof NotEqual
+            && !$node instanceof NotIdentical
+            && !$node instanceof Equal
+            && !$node instanceof Identical
+        ) {
+            return [];
+        }
+
+        $clockType = new ObjectType('App\Dto\Clock');
+        $clock2Type = new ObjectType('App\Dto\Clock');
+
+        $leftType = $scope->getType($node->left);
+        $rightType = $scope->getType($node->right);
+
+        if ($this->containsOnlyTypes($leftType, [$clockType, $clock2Type])) {
+            return ['Cannot compare clocks.'];
+        }
+
+        if ($this->containsOnlyTypes($rightType, [$clockType, $clock2Type])) {
+            return ['Cannot compare clocks.'];
+        }
+
+        if (
+            !$node instanceof Greater
+            && !$node instanceof GreaterOrEqual
+            && !$node instanceof Smaller
+            && !$node instanceof SmallerOrEqual
+            && !$node instanceof Spaceship
+        ) {
+            return [];
+        }
+
+        $leftTypeDescribed = $leftType->describe(VerbosityLevel::typeOnly());
+        $rightTypeDescribed = $rightType->describe(VerbosityLevel::typeOnly());
+
+        if (!$this->isComparable($leftType) || !$this->isComparable($rightType)) {
+            return [
+                "Comparison {$leftTypeDescribed} {$node->getOperatorSigil()} {$rightTypeDescribed} contains " .
+                'non-comparable type, only int|float|string|DateTimeInterface is allowed.'
+            ];
+        }
+
+        if (!$this->isComparableTogether($leftType, $rightType)) {
+            return [
+                "Cannot compare different types in {$leftTypeDescribed} {$node->getOperatorSigil()} " .
+                "{$rightTypeDescribed}."
+            ];
+        }
+
+        return [];
+    }
+
+    private function isComparable(Type $type): bool
+    {
+        $intType = new IntegerType();
+        $floatType = new FloatType();
+        $stringType = new StringType();
+        $dateTimeType = new ObjectType(DateTimeInterface::class);
+
+        return $this->containsOnlyTypes($type, [$intType, $floatType, $stringType, $dateTimeType]);
+    }
+
+    private function isComparableTogether(Type $leftType, Type $rightType): bool
+    {
+        $intType = new IntegerType();
+        $floatType = new FloatType();
+        $stringType = new StringType();
+        $dateTimeType = new ObjectType(DateTimeInterface::class);
+
+        return ($this->containsOnlyTypes($leftType, [$intType, $floatType]) && $this->containsOnlyTypes(
+            $rightType,
+            [$intType, $floatType]
+        ))
+            || ($this->containsOnlyTypes($leftType, [$stringType]) && $this->containsOnlyTypes(
+                $rightType,
+                [$stringType]
+            ))
+            || ($this->containsOnlyTypes($leftType, [$dateTimeType]) && $this->containsOnlyTypes(
+                $rightType,
+                [$dateTimeType]
+            ));
+    }
+
+    /**
+     * @param Type[] $allowedTypes
+     */
+    private function containsOnlyTypes(Type $checkedType, array $allowedTypes): bool
+    {
+        $typesToCheck = $checkedType instanceof UnionType
+            ? $checkedType->getTypes()
+            : [$checkedType];
+
+        foreach ($typesToCheck as $typeToCheck) {
+            $isWithinAllowed = false;
+
+            foreach ($allowedTypes as $allowedType) {
+                if ($allowedType->isSuperTypeOf($typeToCheck)->yes()) {
+                    $isWithinAllowed = true;
+
+                    break;
+                }
+            }
+
+            if (!$isWithinAllowed) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/PHPStan/ValidateClassFieldsReturnsNotNullExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nektria\PHPStan;
+
+use PhpParser\Node\Expr\StaticCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Analyser\SpecifiedTypes;
+use PHPStan\Analyser\TypeSpecifier;
+use PHPStan\Analyser\TypeSpecifierAwareExtension;
+use PHPStan\Analyser\TypeSpecifierContext;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\TypeCombinator;
+
+class ValidateClassFieldsReturnsNotNullExtension implements TypeSpecifierAwareExtension
+{
+    private TypeSpecifier $typeSpecifier;
+
+    public function getClass(): string
+    {
+        return 'App\Utils\Validate';
+    }
+
+    public function isStaticMethodSupported(
+        MethodReflection $staticMethodReflection,
+        StaticCall $node,
+        TypeSpecifierContext $context
+    ): bool {
+        return $staticMethodReflection->getName() === 'classFieldReturnsNotNull' && $context->null();
+    }
+
+    public function specifyTypes(
+        MethodReflection $staticMethodReflection,
+        StaticCall $node,
+        Scope $scope,
+        TypeSpecifierContext $context
+    ): SpecifiedTypes {
+        $expr = $node->getArgs()[2]->value;
+        $typeBefore = $scope->getType($expr);
+        $type = TypeCombinator::removeNull($typeBefore);
+
+        return $this->typeSpecifier->create($expr, $type, TypeSpecifierContext::createTruthy());
+    }
+
+    public function setTypeSpecifier(TypeSpecifier $typeSpecifier): void
+    {
+        $this->typeSpecifier = $typeSpecifier;
+    }
+}
