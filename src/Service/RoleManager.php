0bc4790df3b178d76dba22b78881ff3b6c5a274a
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-03-27 12:52:37 +0100

    auto commit

---
+++ src/Service/RoleManager.php
@@ -0,0 +1,121 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nektria\Service;
+
+use Nektria\Exception\InsufficientCredentialsException;
+
+use function in_array;
+
+class RoleManager
+{
+    public const ROLE_ADMIN = 'ROLE_ADMIN';
+
+    public const ROLE_SYSTEM = 'ROLE_SYSTEM';
+
+    public const ROLE_USER = 'ROLE_USER';
+
+    public const ROLE_GRID_VIEWER = 'ROLE_GRID_VIEWER';
+
+    public const ROLE_DRIVER = 'ROLE_DRIVER';
+
+    public const ROLE_TRAFFIC_CHIEF = 'ROLE_TRAFFIC_CHIEF';
+
+    public const ROLE_TRAFFIC_CHIEF_EXPRESS = 'ROLE_TRAFFIC_CHIEF_EXPRESS';
+
+    public const ROLE_WAREHOUSE_SUPERVISOR = 'ROLE_WAREHOUSE_SUPERVISOR';
+
+    public const ROLE_MARKETING = 'ROLE_MARKETING';
+
+    public const ROLE_ANY = 'ROLE_ANY';
+
+    private const HIERARCHY = [
+        self::ROLE_SYSTEM => [
+            self::ROLE_ADMIN,
+            self::ROLE_USER,
+            self::ROLE_GRID_VIEWER,
+            self::ROLE_WAREHOUSE_SUPERVISOR,
+            self::ROLE_DRIVER,
+            self::ROLE_TRAFFIC_CHIEF,
+            self::ROLE_TRAFFIC_CHIEF_EXPRESS,
+            self::ROLE_MARKETING,
+            self::ROLE_ANY
+        ],
+        self::ROLE_ADMIN => [
+            self::ROLE_SYSTEM,
+            self::ROLE_ADMIN,
+            self::ROLE_USER,
+            self::ROLE_GRID_VIEWER,
+            self::ROLE_WAREHOUSE_SUPERVISOR,
+            self::ROLE_DRIVER,
+            self::ROLE_TRAFFIC_CHIEF,
+            self::ROLE_TRAFFIC_CHIEF_EXPRESS,
+            self::ROLE_MARKETING,
+            self::ROLE_ANY
+        ],
+        self::ROLE_DRIVER => [
+            self::ROLE_ANY
+        ],
+        self::ROLE_GRID_VIEWER => [
+            self::ROLE_ANY
+        ],
+        self::ROLE_TRAFFIC_CHIEF => [
+            self::ROLE_TRAFFIC_CHIEF_EXPRESS,
+            self::ROLE_DRIVER,
+            self::ROLE_ANY
+        ],
+        self::ROLE_TRAFFIC_CHIEF_EXPRESS => [
+            self::ROLE_TRAFFIC_CHIEF,
+            self::ROLE_DRIVER,
+            self::ROLE_ANY
+        ],
+        self::ROLE_USER => [
+            self::ROLE_GRID_VIEWER,
+            self::ROLE_WAREHOUSE_SUPERVISOR,
+            self::ROLE_DRIVER,
+            self::ROLE_TRAFFIC_CHIEF,
+            self::ROLE_TRAFFIC_CHIEF_EXPRESS,
+            self::ROLE_MARKETING,
+            self::ROLE_ANY
+        ],
+        self::ROLE_WAREHOUSE_SUPERVISOR => [self::ROLE_GRID_VIEWER, self::ROLE_ANY],
+        self::ROLE_MARKETING => [self::ROLE_ANY]
+    ];
+
+    /**
+     * @param string[] $targetRoles
+     */
+    public function checkAtLeast(string $role, array $targetRoles): bool
+    {
+        if ($role === self::ROLE_ADMIN) {
+            return true;
+        }
+
+        if ($role === self::ROLE_SYSTEM) {
+            return true;
+        }
+
+        if (!isset(self::HIERARCHY[$role])) {
+            throw new InsufficientCredentialsException();
+        }
+
+        if (in_array(self::ROLE_ANY, $targetRoles, true)) {
+            return true;
+        }
+
+        if (in_array($role, $targetRoles, true)) {
+            return true;
+        }
+
+        foreach ($targetRoles as $targetRole) {
+            $roleIsGranted = $targetRole === $role || in_array($targetRole, self::HIERARCHY[$role], true);
+
+            if ($roleIsGranted) {
+                return true;
+            }
+        }
+
+        throw new InsufficientCredentialsException();
+    }
+}
