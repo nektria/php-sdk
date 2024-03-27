738fd6f70e71ea78ef251802f342efaf11d0fda6
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-03-27 10:17:59 +0100

    auto commit

--- src/Service/SharedTenantCache.php
+++ src/Service/SharedTenantCache.php
@@ -8,48 +8,52 @@ use Nektria\Infrastructure\SharedRedisCache;
 
 /**
  * @phpstan-type WarehouseMetadataArray array{
- *         id: string,
- *         name: string,
- *         metadata: array{
- *             autoDuplicateLastWeek: bool,
- *             availableTags: string[],
- *             blockWarehouse: bool,
- *             dayOffExtendsCutoff: bool,
- *             deliveryTime: number | null,
- *             ecoMode: string,
- *             expressGridDisabled: bool,
- *             extendPickingShiftsDisabled: bool,
- *             extraLongSpeed: number,
- *             forceDriverAssignation: bool,
- *             forceTags: bool,
- *             gridMode: string,
- *             gridVersion: number,
- *             gridViewerOrdersPrefix: string,
- *             ignoreRoutesOnLogsList: string[],
- *             importOrdersFromFileByProxy: bool,
- *             longSpeed: number,
- *             nextStepEnabled: bool,
- *             parkingTime: number,
- *             proxyHost: string,
- *             proxyToken: string,
- *             recoverCoords: bool,
- *             sendNewOrderToProxy: bool,
- *             sendRoutesAtPickingShiftClosesAt: bool,
- *             sendRoutesByProxy: bool,
- *             shortSpeed: number,
- *             syncRMORder: bool,
- *             syncRMShift: bool,
- *             syncRMWarehouse: bool,
- *             useAddressInsteadOfShopperCode: bool,
- *         }
+ *         autoDuplicateLastWeek: bool,
+ *         availableTags: string[],
+ *         blockWarehouse: bool,
+ *         dayOffExtendsCutoff: bool,
+ *         deliveryTime: number | null,
+ *         ecoMode: string,
+ *         expressGridDisabled: bool,
+ *         extendPickingShiftsDisabled: bool,
+ *         extraLongSpeed: number,
+ *         forceDriverAssignation: bool,
+ *         forceTags: bool,
+ *         gridMode: string,
+ *         gridVersion: number,
+ *         gridViewerOrdersPrefix: string,
+ *         ignoreRoutesOnLogsList: string[],
+ *         importOrdersFromFileByProxy: bool,
+ *         longSpeed: number,
+ *         nextStepEnabled: bool,
+ *         parkingTime: number,
+ *         proxyHost: string,
+ *         proxyToken: string,
+ *         recoverCoords: bool,
+ *         sendNewOrderToProxy: bool,
+ *         sendRoutesAtPickingShiftClosesAt: bool,
+ *         sendRoutesByProxy: bool,
+ *         shortSpeed: number,
+ *         syncRMORder: bool,
+ *         syncRMShift: bool,
+ *         syncRMWarehouse: bool,
+ *         useAddressInsteadOfShopperCode: bool,
  *     }
  *
- * @extends SharedRedisCache<WarehouseMetadataArray>
+ * @extends SharedRedisCache<array{
+ *     id: string,
+ *     name: string,
+ *     metadata: WarehouseMetadataArray
+ * }>
  */
 class SharedTenantCache extends SharedRedisCache
 {
     /**
-     * @return WarehouseMetadataArray|null
+     * @return array{
+     *      id: string,
+     *      name: string,
+     *      metadata: WarehouseMetadataArray
+     *  }|null
      */
     public function read(string $key): ?array
     {
@@ -57,7 +61,11 @@ class SharedTenantCache extends SharedRedisCache
     }
 
     /**
-     * @param WarehouseMetadataArray $tenant
+     * @param array{
+     *      id: string,
+     *      name: string,
+     *      metadata: WarehouseMetadataArray
+     *  } $tenant
      */
     public function save(string $key, array $tenant): void
     {
