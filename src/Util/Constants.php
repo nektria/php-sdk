21582fdfb5aa79232b45f2e8e78fc49c199a005b
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-04-11 18:26:39 +0200

    auto commit

---
+++ src/Util/Constants.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nektria\Util;
+
+class Constants
+{
+    public const ORDER_TYPE_ORDER = 'ORDER';
+
+    public const ORDER_TYPE_EXPRESS_ORDER = 'EXPRESS_ORDER';
+
+    public const ORDER_TYPE_STORE_PICK_UP_ORDER = 'STORE_PICK_UP_ORDER';
+}
