6efe61df7a45584aeea9616d8849a7dcf6861831
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-05-23 17:56:03 +0200

    auto commit

---
+++ src/Listener/ConsoleListener.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nektria\Listener;
+
+use Nektria\Console\Console;
+use Nektria\Document\ThrowableDocument;
+use Nektria\Service\AlertService;
+use Nektria\Service\ContextService;
+use Nektria\Service\VariableCache;
+use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\Console\Event\ConsoleErrorEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+abstract class ConsoleListener implements EventSubscriberInterface
+{
+    public function __construct(
+        private readonly ContextService $contextService,
+        private readonly VariableCache $variableCache,
+        private readonly AlertService $alertService,
+    ) {
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ConsoleEvents::ERROR => 'onConsoleError',
+        ];
+    }
+
+    public function onConsoleError(ConsoleErrorEvent $event): void
+    {
+        $command = $event->getCommand();
+
+        if ($command instanceof Console || $command === null) {
+            return;
+        }
+
+        $path = '/Console/' . str_replace('\\', '/', $command::class);
+
+        if ($this->contextService->env() === ContextService::DEV || $this->variableCache->refreshKey($path)) {
+            $tenantName = 'none';
+
+            $args = $event->getInput()->getArguments();
+            $options = $event->getInput()->getOptions();
+
+            $method = 'CONSOLE';
+            $this->alertService->sendThrowable(
+                $tenantName,
+                $method,
+                $path,
+                [
+                    'command' => $args['command'],
+                    'arguments' => $args['receivers'],
+                    'options' => $options,
+                ],
+                new ThrowableDocument($event->getError()),
+            );
+        }
+    }
+}
