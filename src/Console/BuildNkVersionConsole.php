c296c79db1ae4a53f95b8d2f9791213efb5a8c0c
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-05-25 08:39:38 +0200

    auto commit

--- src/Console/BuildNkVersionConsole.php
+++ src/Console/BuildNkVersionConsole.php
@@ -25,12 +25,12 @@ class BuildNkVersionConsole extends Console
 
     protected function play(): void
     {
-        $commit = substr(StringUtil::trim((string) exec('git rev-parse HEAD')), 0, 7);
-        $total = exec('git rev-list --count HEAD');
-        $branch = $this->input()->getArgument('branch') ?? exec('git rev-parse --abbrev-ref HEAD');
-        $version = $branch === 'master' ? "v{$total}" : "v{$total}-{$branch}";
-
         try {
+            $commit = substr(StringUtil::trim((string) exec('git rev-parse HEAD')), 0, 7);
+            $total = exec('git rev-list --count HEAD');
+            $branch = $this->input()->getArgument('branch') ?? exec('git rev-parse --abbrev-ref HEAD');
+            $version = $branch === 'master' ? "v{$total}" : "v{$total}-{$branch}";
+
             $this->output()->writeln(JsonUtil::encode([
                 'builtAt' => Clock::now()->iso8601String('Europe/Madrid'),
                 'hash' => $commit,
