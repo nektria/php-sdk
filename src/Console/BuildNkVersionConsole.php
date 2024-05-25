26a9fa5b54821dc1d10079eb6afc1ba4c85a308b
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-05-25 08:38:22 +0200

    auto commit

--- src/Console/BuildNkVersionConsole.php
+++ src/Console/BuildNkVersionConsole.php
@@ -8,6 +8,7 @@ use Nektria\Dto\Clock;
 use Nektria\Service\ContextService;
 use Nektria\Util\JsonUtil;
 use Nektria\Util\StringUtil;
+use Throwable;
 
 class BuildNkVersionConsole extends Console
 {
@@ -29,12 +30,22 @@ class BuildNkVersionConsole extends Console
         $branch = $this->input()->getArgument('branch') ?? exec('git rev-parse --abbrev-ref HEAD');
         $version = $branch === 'master' ? "v{$total}" : "v{$total}-{$branch}";
 
-        $this->output()->writeln(JsonUtil::encode([
-            'builtAt' => Clock::now()->iso8601String('Europe/Madrid'),
-            'hash' => $commit,
-            'project' => $this->contextService->project(),
-            'type' => 'Release',
-            'version' => $version,
-        ], true));
+        try {
+            $this->output()->writeln(JsonUtil::encode([
+                'builtAt' => Clock::now()->iso8601String('Europe/Madrid'),
+                'hash' => $commit,
+                'project' => $this->contextService->project(),
+                'type' => 'Release',
+                'version' => $version,
+            ], true));
+        } catch (Throwable) {
+            $this->output()->writeln(JsonUtil::encode([
+                'builtAt' => Clock::now()->iso8601String('Europe/Madrid'),
+                'hash' => '',
+                'project' => $this->contextService->project(),
+                'type' => 'Development',
+                'version' => '',
+            ], true));
+        }
     }
 }
