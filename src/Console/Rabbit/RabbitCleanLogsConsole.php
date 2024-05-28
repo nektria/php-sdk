d6e4ee975f287b4e8000dbe9f64ae3aa13e98e51
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-05-28 11:02:39 +0200

    auto commit

--- src/Console/Rabbit/RabbitCleanLogsConsole.php
+++ src/Console/Rabbit/RabbitCleanLogsConsole.php
@@ -26,6 +26,32 @@ class RabbitCleanLogsConsole extends Console
             return;
         }
 
+        $this->cleanProcessingMessages();
+        $this->cleanPendingMessages();
+    }
+
+    private function cleanPendingMessages(): void
+    {
+        $data = JsonUtil::decode($this->sharedVariableCache->readString('bus_messages_pending', '[]'));
+
+        $projects = [];
+        foreach ($data as $item) {
+            [$project, $clzz] = explode('_', $item);
+
+            $projects[$project] ??= [];
+            $projects[$project][] = $clzz;
+        }
+
+        ksort($projects);
+        foreach ($projects as $project => $clzzs) {
+            foreach ($clzzs as $clzz) {
+                $this->sharedVariableCache->saveInt("bus_messages_pending_{$project}_{$clzz}", 0, ttl: 3600);
+            }
+        }
+    }
+
+    private function cleanProcessingMessages(): void
+    {
         $data = JsonUtil::decode($this->sharedVariableCache->readString('bus_messages', '[]'));
 
         $projects = [];
