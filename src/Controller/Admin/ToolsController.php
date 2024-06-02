91ea6152c127304ec7e1a2b3f9d22901516b6b14
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-06-03 00:50:01 +0200

    auto commit

--- src/Controller/Admin/ToolsController.php
+++ src/Controller/Admin/ToolsController.php
@@ -23,8 +23,65 @@ use const STR_PAD_LEFT;
 #[Route('/api/admin/tools')]
 class ToolsController extends Controller
 {
+    #[Route('/debug', methods: ['PATCH'])]
+    public function configureDebug(ContextService $contextService): JsonResponse
+    {
+        $enable = $this->requestData->retrieveBool('enable');
+        $lifetime = $this->requestData->getInt('lifetime') ?? 3600;
+        $projects = $this->requestData->retrieveStringArray('projects');
+
+        $contextService->setDebugMode($enable, $projects, $lifetime);
+
+        return $this->emptyResponse();
+    }
+
+    #[Route('/decrypt', methods: 'PATCH')]
+    public function decodeAllVariables(ContextService $contextService): JsonResponse
+    {
+        if (!$contextService->isLocalEnvironament()) {
+            throw new InsufficientCredentialsException();
+        }
+
+        $pass = $this->requestData->getString('pass') ?? $contextService->project();
+
+        return $this->documentResponse(new ArrayDocument(JsonUtil::decode(
+            (string) base64_decode(
+                (string) openssl_decrypt(
+                    $this->requestData->retrieveString('hash'),
+                    'AES-256-CBC',
+                    $pass,
+                    0,
+                    str_pad($pass, 16, '0', STR_PAD_LEFT),
+                ),
+                true
+            )
+        )));
+    }
+
+    #[Route('/rabbit/delete', methods: 'PATCH')]
+    public function deleteARabbitQueue(RequestClient $requestClient, string $rabbitDsn): DocumentResponse
+    {
+        $queue = $this->requestData->retrieveString('queue');
+        $host = str_replace(['amqp', '5672'], ['http', '15672'], $rabbitDsn);
+
+        $content = $requestClient->get("{$host}/api/queues/%2F/{$queue}")->json();
+
+        $vhost = urlencode($content['vhost']);
+
+        $requestClient->delete(
+            "{$host}/api/queues/{$vhost}/{$queue}",
+            data: [
+                'vhost' => '/',
+                'name' => $queue,
+                'mode' => 'delete',
+            ]
+        );
+
+        return $this->emptyResponse();
+    }
+
     #[Route('/console', methods: ['PATCH'])]
-    public function console(): Response
+    public function executeAConsoleCommand(): Response
     {
         $command = $this->requestData->retrieveString('command');
         if (!str_starts_with($command, 'admin:') && !str_starts_with($command, 'sdk:')) {
@@ -39,8 +96,60 @@ class ToolsController extends Controller
         return $this->buildResponseForProcess($command);
     }
 
+    #[Route('/database/migrations', methods: ['GET'])]
+    public function executeDoctrineMigrationStatus(): Response
+    {
+        $command = new Process(array_merge(['../bin/console', 'doctrine:migration:status']));
+        $command->run();
+
+        return $this->buildResponseForProcess($command);
+    }
+
+    #[Route('/database/schema', methods: ['GET'])]
+    public function executeDoctrineSchemaUpdateDumpSql(): Response
+    {
+        $command = new Process(array_merge(['../bin/console', 'doctrine:schema:update', '--dump-sql']));
+        $command->run();
+
+        return $this->buildResponseForProcess($command);
+    }
+
+    #[Route('/debug/status', methods: ['PATCH'])]
+    public function getDebugConfigurationStatus(ContextService $contextService): JsonResponse
+    {
+        return new JsonResponse($contextService->debugModes(
+            $this->requestData->retrieveStringArray('projects'),
+        ));
+    }
+
+    #[Route('/rabbit/status', methods: 'GET')]
+    public function getRabbitStatus(RequestClient $requestClient, string $rabbitDsn): DocumentResponse
+    {
+        $host = str_replace(['amqp', '5672'], ['http', '15672'], $rabbitDsn);
+
+        $content = $requestClient->get(
+            "{$host}/api/queues",
+        )->json();
+
+        $data = [];
+        foreach ($content as $queue) {
+            $name = $queue['name'];
+            $ready = (int) $queue['messages_ready'];
+            $unacked = (int) $queue['messages_unacknowledged'];
+            $speed = (float) $queue['messages_unacknowledged_details']['rate'];
+
+            $data[$name] = [
+                'ready' => $ready,
+                'unacknowledged' => $unacked,
+                'rate' => $speed,
+            ];
+        }
+
+        return $this->documentResponse(new ArrayDocument($data));
+    }
+
     #[Route('/crypt', methods: 'GET')]
-    public function crypt(ContextService $contextService): Response
+    public function readAllVariables(ContextService $contextService): Response
     {
         $lines = explode("\n", FileUtil::read('/app/.env'));
         $envs = [];
@@ -70,17 +179,8 @@ class ToolsController extends Controller
         return new Response((string) $result);
     }
 
-    #[Route('/database/migrations', methods: ['GET'])]
-    public function databaseMigrations(): Response
-    {
-        $command = new Process(array_merge(['../bin/console', 'doctrine:migration:status']));
-        $command->run();
-
-        return $this->buildResponseForProcess($command);
-    }
-
     #[Route('/database/read', methods: 'GET')]
-    public function databaseRead(ArrayDocumentReadModel $readModel): DocumentResponse
+    public function readFromDatabase(ArrayDocumentReadModel $readModel): DocumentResponse
     {
         return $this->documentResponse(
             $readModel->readCustom(
@@ -89,106 +189,6 @@ class ToolsController extends Controller
                 $this->requestData->getInt('page') ?? 1,
             ),
         );
-    }
-
-    #[Route('/database/schema', methods: ['GET'])]
-    public function databaseSchema(): Response
-    {
-        $command = new Process(array_merge(['../bin/console', 'doctrine:schema:update', '--dump-sql']));
-        $command->run();
-
-        return $this->buildResponseForProcess($command);
-    }
-
-    #[Route('/debug', methods: ['PATCH'])]
-    public function debug(ContextService $contextService): JsonResponse
-    {
-        $enable = $this->requestData->retrieveBool('enable');
-        $lifetime = $this->requestData->getInt('lifetime') ?? 3600;
-        $projects = $this->requestData->retrieveStringArray('projects');
-
-        $contextService->setDebugMode($enable, $projects, $lifetime);
-
-        return $this->emptyResponse();
-    }
-
-    #[Route('/debug/status', methods: ['PATCH'])]
-    public function debugStatus(ContextService $contextService): JsonResponse
-    {
-        return new JsonResponse($contextService->debugModes(
-            $this->requestData->retrieveStringArray('projects'),
-        ));
-    }
-
-    #[Route('/decrypt', methods: 'PATCH')]
-    public function decrypt(ContextService $contextService): JsonResponse
-    {
-        if (!$contextService->isLocalEnvironament()) {
-            throw new InsufficientCredentialsException();
-        }
-
-        $pass = $this->requestData->getString('pass') ?? $contextService->project();
-
-        return $this->documentResponse(new ArrayDocument(JsonUtil::decode(
-            (string) base64_decode(
-                (string) openssl_decrypt(
-                    $this->requestData->retrieveString('hash'),
-                    'AES-256-CBC',
-                    $pass,
-                    0,
-                    str_pad($pass, 16, '0', STR_PAD_LEFT),
-                ),
-                true
-            )
-        )));
-    }
-
-    #[Route('/rabbit/delete', methods: 'PATCH')]
-    public function rabbitDelete(RequestClient $requestClient, string $rabbitDsn): DocumentResponse
-    {
-        $queue = $this->requestData->retrieveString('queue');
-        $host = str_replace(['amqp', '5672'], ['http', '15672'], $rabbitDsn);
-
-        $content = $requestClient->get("{$host}/api/queues/%2F/{$queue}")->json();
-
-        $vhost = urlencode($content['vhost']);
-
-        $requestClient->delete(
-            "{$host}/api/queues/{$vhost}/{$queue}",
-            data: [
-                'vhost' => '/',
-                'name' => $queue,
-                'mode' => 'delete',
-            ]
-        );
-
-        return $this->emptyResponse();
-    }
-
-    #[Route('/rabbit/status', methods: 'GET')]
-    public function rabbitStatus(RequestClient $requestClient, string $rabbitDsn): DocumentResponse
-    {
-        $host = str_replace(['amqp', '5672'], ['http', '15672'], $rabbitDsn);
-
-        $content = $requestClient->get(
-            "{$host}/api/queues",
-        )->json();
-
-        $data = [];
-        foreach ($content as $queue) {
-            $name = $queue['name'];
-            $ready = (int) $queue['messages_ready'];
-            $unacked = (int) $queue['messages_unacknowledged'];
-            $speed = (float) $queue['messages_unacknowledged_details']['rate'];
-
-            $data[$name] = [
-                'ready' => $ready,
-                'unacknowledged' => $unacked,
-                'rate' => $speed,
-            ];
-        }
-
-        return $this->documentResponse(new ArrayDocument($data));
     }
 
     private function buildResponseForProcess(Process $process): Response
