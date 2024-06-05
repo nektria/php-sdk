7192e2e75ffa3570d37a5dc5a91c59d16cb2e2c3
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-06-05 09:41:28 +0200

    auto commit

--- src/Exception/NektriaRuntimeException.php
+++ src/Exception/NektriaRuntimeException.php
@@ -5,10 +5,17 @@ declare(strict_types=1);
 namespace Nektria\Exception;
 
 use RuntimeException;
+use Throwable;
 
 abstract class NektriaRuntimeException extends RuntimeException
 {
-    private bool $silent = false;
+    private bool $silent;
+
+    public function __construct(string $message = '', int $code = 0, ?Throwable $previous = null, bool $silent = false)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->silent = $silent;
+    }
 
     public function makeSilent(): void
     {
