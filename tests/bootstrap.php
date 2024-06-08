9360120cab3c94ef2a2bdee781cd00ef71ba3883
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-06-08 15:41:12 +0200

    auto commit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 use Symfony\Component\ErrorHandler\ErrorHandler;
 
-require '/app/vendor/autoload.php';
+require './vendor/autoload.php';
 
 set_exception_handler([new ErrorHandler(), 'handleException']);
