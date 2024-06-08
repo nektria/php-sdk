b4d9881aa0dfad689172ef816b95d45fcdf8a8ed
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-06-08 15:58:24 +0200

    auto commit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 use Symfony\Component\ErrorHandler\ErrorHandler;
 
-if (is_dir('/app')) {
-    require '/app/vendor/autoload.php';
-} else {
-    require '/workspace/vendor/autoload.php';
-}
+require getcwd() . '/vendor/autoload.php';
 
 set_exception_handler([new ErrorHandler(), 'handleException']);
