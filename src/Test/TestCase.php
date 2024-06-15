63c36c0805e3776ccf06a0a2aac011acf8cc8cca
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-06-15 11:16:32 +0200

    auto commit

--- src/Test/TestCase.php
+++ src/Test/TestCase.php
@@ -111,7 +111,7 @@ class TestCase extends WebTestCase
             throw new NektriaException('Test function starting with "test" has not been found.');
         }
 
-        $key = str_replace('::', '_', $key);
+        $key = str_replace('::', '__', $key);
 
         return str_replace('\\', '_', $key);
     }
