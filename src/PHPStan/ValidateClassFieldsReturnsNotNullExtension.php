324930aae9f198e5bcdc287430ff1a85210db756
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-06-16 02:18:37 +0200

    auto commit

--- src/PHPStan/ValidateClassFieldsReturnsNotNullExtension.php
+++ src/PHPStan/ValidateClassFieldsReturnsNotNullExtension.php
@@ -5,13 +5,17 @@ declare(strict_types=1);
 namespace Nektria\PHPStan;
 
 use Nektria\Util\Validate;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Expr\PropertyFetch;
 use PhpParser\Node\Expr\StaticCall;
+use PhpParser\Node\Scalar\String_;
 use PHPStan\Analyser\Scope;
 use PHPStan\Analyser\SpecifiedTypes;
 use PHPStan\Analyser\TypeSpecifier;
 use PHPStan\Analyser\TypeSpecifierAwareExtension;
 use PHPStan\Analyser\TypeSpecifierContext;
 use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\NullType;
 use PHPStan\Type\TypeCombinator;
 
 class ValidateClassFieldsReturnsNotNullExtension implements TypeSpecifierAwareExtension
@@ -28,7 +32,7 @@ class ValidateClassFieldsReturnsNotNullExtension implements TypeSpecifierAwareEx
         StaticCall $node,
         TypeSpecifierContext $context
     ): bool {
-        return $staticMethodReflection->getName() === 'classFieldReturnsNotNull' && $context->null();
+        return $staticMethodReflection->getName() === 'classFieldsReturnsNotNull' && $context->null();
     }
 
     public function setTypeSpecifier(TypeSpecifier $typeSpecifier): void
@@ -42,10 +46,30 @@ class ValidateClassFieldsReturnsNotNullExtension implements TypeSpecifierAwareEx
         Scope $scope,
         TypeSpecifierContext $context
     ): SpecifiedTypes {
-        $expr = $node->getArgs()[2]->value;
-        $typeBefore = $scope->getType($expr);
-        $type = TypeCombinator::removeNull($typeBefore);
+        $fields = $node->getArgs()[2]->value;
+        $objectExpr = $node->getArgs()[0]->value;
+        $objectType = $scope->getType($objectExpr);
 
-        return $this->typeSpecifier->create($expr, $type, TypeSpecifierContext::createTruthy());
+        if (!$fields instanceof Array_) {
+            return new SpecifiedTypes();
+        }
+
+        $specifiedTypes = new SpecifiedTypes();
+        foreach ($fields->items as $fieldItem) {
+            if ($fieldItem->value instanceof String_) {
+                $fieldName = $fieldItem->value->value;
+                $propertyType = $objectType->getProperty($fieldName, $scope)->getReadableType();
+
+                if ($propertyType->isSuperTypeOf(new NullType())->yes()) {
+                    $newType = TypeCombinator::removeNull($propertyType);
+                    $propertyExpr = new PropertyFetch($objectExpr, $fieldName);
+                    $specifiedTypes = $specifiedTypes->unionWith(
+                        $this->typeSpecifier->create($propertyExpr, $newType, TypeSpecifierContext::createTruthy())
+                    );
+                }
+            }
+        }
+
+        return $specifiedTypes;
     }
 }
