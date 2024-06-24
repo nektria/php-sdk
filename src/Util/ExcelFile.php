88221af8a871be2f4ccb0b4e2c8eb8248283bd5b
Javier Gonzalez Cuadrado <xgc1986@gmail.com>
2024-06-24 18:41:28 +0200

    auto commit

--- src/Util/ExcelFile.php
+++ src/Util/ExcelFile.php
@@ -6,35 +6,33 @@ namespace Nektria\Util;
 
 use Nektria\Dto\Clock;
 use Nektria\Dto\LocalClock;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Nektria\Exception\NektriaException;
+use XLSXWriter;
 
 class ExcelFile
 {
-    private Worksheet $worksheet;
+    private string $worksheet;
 
     private function __construct(
         private readonly string $file,
-        private readonly Spreadsheet $spreadsheet,
+        private readonly XLSXWriter $spreadsheet,
     ) {
-        $this->worksheet = $this->spreadsheet->getActiveSheet();
+        $this->worksheet = 'Sheet1';
     }
 
     public static function new(string $file): self
     {
-        return new self($file, new Spreadsheet());
+        return new self($file, new XLSXWriter());
     }
 
     public function save(): void
     {
-        $writer = new Xlsx($this->spreadsheet);
-        $writer->save($this->file);
+        $this->spreadsheet->writeToFile($this->file);
     }
 
     public function setCell(int $row, int $column, string $value): void
     {
-        $this->worksheet->setCellValue([$column, $row], $value);
+        throw new NektriaException('Not implemented');
     }
 
     /**
@@ -42,19 +40,7 @@ class ExcelFile
      */
     public function setCol(int $row, int $column, array $values): void
     {
-        foreach ($values as $i => $iValue) {
-            $this->setCell($row + $i, $column, $iValue);
-        }
-    }
-
-    /**
-     * @param string[][] $values
-     */
-    public function setColRow(int $row, int $column, array $values): void
-    {
-        foreach ($values as $i => $iValue) {
-            $this->setCol($row, $column + $i, $iValue);
-        }
+        throw new NektriaException('Not implemented');
     }
 
     /**
@@ -62,9 +48,7 @@ class ExcelFile
      */
     public function setRow(int $row, int $column, array $values): void
     {
-        foreach ($values as $i => $iValue) {
-            $this->setCell($row, $column + $i, $iValue);
-        }
+        $this->spreadsheet->writeSheetRow($this->worksheet, $values);
     }
 
     public function transformToDate(Clock | LocalClock $clock): string
