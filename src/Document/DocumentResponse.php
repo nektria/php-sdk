fc0f9099448ce611d0e4b88fd8535ba81f2caa00
Javier Gonzalez <xgc1986@gmail.com>
2024-06-30 12:22:47 +0200

    auto commit

--- src/Document/DocumentResponse.php
+++ src/Document/DocumentResponse.php
@@ -9,25 +9,30 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DocumentResponse extends JsonResponse
 {
-    public readonly Document $document;
-
-    public function __construct(Document $document, ContextService $context, int $status = 200)
-    {
-        if ($document instanceof ThrowableDocument) {
-            parent::__construct($document->toArray($context), $document->status);
+    /**
+     * @param array<string, string> $headers
+     */
+    public function __construct(
+        public readonly Document $document,
+        ContextService $context,
+        int $status = 200,
+        array $headers = []
+    ) {
+        if ($this->document instanceof ThrowableDocument) {
+            parent::__construct($this->document->toArray($context), $this->document->status);
         } elseif (
-            $document instanceof DocumentCollection
+            $this->document instanceof DocumentCollection
             && (
                 $context->context() === ContextService::PUBLIC_V2
             )
         ) {
             parent::__construct([
-                'list' => $document->toArray($context),
+                'list' => $this->document->toArray($context),
             ], $status);
         } else {
-            parent::__construct($document->toArray($context), $status);
+            parent::__construct($this->document->toArray($context), $status);
         }
 
-        $this->document = $document;
+        $this->headers->add($headers);
     }
 }
