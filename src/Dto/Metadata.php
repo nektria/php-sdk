39d2011564f102e7664f28df449c668177ac1912
Javier Gonzalez <xgc1986@gmail.com>
2024-11-27 17:16:02 +0100

    auto commit

---
+++ src/Dto/Metadata.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nektria\Dto;
+
+use Nektria\Document\Document;
+use Nektria\Service\ContextService;
+use Nektria\Util\ArrayUtil;
+
+readonly class Metadata extends Document
+{
+    /** @var mixed[] */
+    private array $data;
+
+    /**
+     * @param mixed[] $data
+     */
+    final public function __construct(
+        array $data
+    ) {
+        $this->data = $data;
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function data(): array
+    {
+        return $this->data;
+    }
+
+    public function getField(string $field): mixed
+    {
+        return $this->data[$field] ?? null;
+    }
+
+    /**
+     * @param mixed[] $data
+     */
+    public function mergeData(array $data): self
+    {
+        if (ArrayUtil::isSubset($data, $this->data)) {
+            return $this;
+        }
+
+        return new self([...$this->data, ...$data]);
+    }
+
+    public function toArray(ContextService $context): array
+    {
+        return $this->data;
+    }
+
+    public function updateField(string $field, mixed $value): self
+    {
+        if ($this->getField($field) === $value) {
+            return $this;
+        }
+
+        $data = [...$this->data];
+        $data[$field] = $value;
+
+        return new self($data);
+    }
+}
