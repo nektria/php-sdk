84dd9e50dfed57caf7fd194fd40d72905875bf09
Javier Gonzalez <xgc1986@gmail.com>
2024-12-30 11:12:27 +0100

    increase version

--- src/Controller/Common/PostmanController.php
+++ src/Controller/Common/PostmanController.php
@@ -640,7 +640,7 @@ readonly class PostmanController extends Controller
                 'Length' => [],
                 'String' => 'string',
                 'StringArray' => ['string'],
-                'Metadata'  => [],
+                'Metadata' => [],
                 default => '?',
             };
 
