acda968b7078c2d219b3e3bc5e6696a25eb2db05
Javier Gonzalez <xgc1986@gmail.com>
2025-03-15 15:50:28 +0100

    auto commit

--- src/Listener/RequestListener.php
+++ src/Listener/RequestListener.php
@@ -227,7 +227,7 @@ abstract class RequestListener implements EventSubscriberInterface
             $fileResponse->deleteFileAfterSend();
             $fileResponse->headers->set('Content-Type', $response->document->mime);
             if ($response->document->maxAge !== null) {
-                $fileResponse->headers->set('Cache-Control', $response->document->maxAge );
+                $fileResponse->headers->set('Cache-Control', "{$response->document->maxAge}" );
             }
             $fileResponse->setContentDisposition(
                 ResponseHeaderBag::DISPOSITION_ATTACHMENT,
