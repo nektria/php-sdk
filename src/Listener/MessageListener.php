b8fdccaee608c2e1f7f0889084469535af170adf
Javier Gonzalez <xgc1986@gmail.com>
2025-03-30 00:32:20 +0100

    auto commit

--- src/Listener/MessageListener.php
+++ src/Listener/MessageListener.php
@@ -210,7 +210,7 @@ abstract class MessageListener implements EventSubscriberInterface
                         if ($sendAlert && !$silent) {
                             $this->alertService->sendThrowable(
                                 $this->securityService->currentUser()?->tenant->name ?? 'none',
-                                'RABBIT ' . $originalException::class,
+                                'RABBIT ' . $originalException::class . ' ' . $exception->getCode(),
                                 "/{$messageClass}/{$message->ref()}",
                                 $data,
                                 $exception,
