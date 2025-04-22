bac15c1dc64c95d90653989bea518160048bd827
Javier Gonzalez <xgc1986@gmail.com>
2025-04-22 16:40:27 +0200

    auto commit

--- assets/entity/src/Service/__ENTITY__Service.php
+++ assets/entity/src/Service/__ENTITY__Service.php
@@ -7,12 +7,14 @@ namespace App\Service;
 use App\Entity\__ENTITY__;
 use App\Infrastructure\WriteModel\__ENTITY__WriteModel;
 use Nektria\Exception\ResourceNotFoundException;
+use Nektria\Service\AbstractService;
 
-readonly class __ENTITY__Service
+readonly class __ENTITY__Service extends AbstractService
 {
     public function __construct(
         private __ENTITY__WriteModel $__ENTITY_CC__WriteModel,
     ) {
+        parent::__construct();
     }
 
     public function delete(__ENTITY__ $__ENTITY_CC__): void
