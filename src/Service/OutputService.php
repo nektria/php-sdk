fb093b8d613fb5af5b05202c55fbf070a949931f
Javier Gonzalez <xgc1986@gmail.com>
2025-10-16 16:49:20 +0200

    auto commit

---
+++ src/Service/OutputService.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Nektria\Service;
+
+use Nektria\Dto\Clock;
+use Nektria\Dto\LocalClock;
+use RuntimeException;
+use Symfony\Component\Console\Cursor;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class OutputService
+{
+    private readonly LocalClock $createdAt;
+    private Cursor|null $cursor;
+    private string $logFile;
+    private OutputInterface|null $output;
+
+    public function __construct()
+    {
+        $this->createdAt = Clock::now()->toLocal('Europe/Madrid');
+        $this->logFile = "tmp/{$this->createdAt->dateTimeString()}.log";
+    }
+
+    public function assignOutput(OutputInterface $outputInterface): void
+    {
+        $this->output = $outputInterface;
+        $this->cursor = new Cursor($this->output);
+    }
+
+    public function write(string|int|float|bool $output): void
+    {
+        if ($output === true) {
+            $output = 'true';
+        }
+
+        if ($output === false) {
+            $output = 'false';
+        }
+
+        $now = Clock::now()->toLocal('Europe/Madrid');
+        $cleanOutput = preg_replace('/<\/?\w+\d*>/', '', $output);
+        $formattedOutput = "[{$now->microDateTimeString()}] {$cleanOutput}";
+        file_put_contents($this->logFile, $formattedOutput, FILE_APPEND);
+
+        if ($this->output !== null) {
+            file_put_contents($this->logFile, $output, FILE_APPEND);
+        }
+    }
+
+    public function writeln(string|int|float|bool $output): void
+    {
+        if ($output === true) {
+            $output = 'true';
+        }
+
+        if ($output === false) {
+            $output = 'false';
+        }
+
+        $this->write($output . PHP_EOL);
+    }
+
+    protected function clearLine(): void
+    {
+        if ($this->cursor === null) {
+            return;
+        }
+
+        $this->cursor->clearLine();
+    }
+
+    protected function clearPreviousLine(bool $clearCurrentLine = true): void
+    {
+        if ($this->cursor === null) {
+            return;
+        }
+
+        if ($clearCurrentLine) {
+            $this->cursor->clearLine();
+        }
+
+        $this->cursor->moveUp();
+        $this->cursor->clearLine();
+    }
+}
