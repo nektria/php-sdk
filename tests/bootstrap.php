58ade703a2a610106fbe760c8f4de36075c315bc
Javi <javi@linux.xgc>
2025-10-22 13:18:43 +0200

    auto commit

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Symfony\Component\ErrorHandler\ErrorHandler;
-
-require getcwd() . '/vendor/autoload.php';
-
-set_exception_handler([new ErrorHandler(), 'handleException']);
