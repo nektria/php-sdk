48fd47cc4e984659d601f3bf9fdc5b5998d2a187
xgc <git@xgc.es>
2026-05-22 18:09:20 +0200

    auto commit

---
+++ src/Document/PaginatedDocumentCollection.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nektria\Document;
+
+use Nektria\Service\ContextService;
+
+/**
+ * @template T of Document
+ */
+readonly class PaginatedDocumentCollection extends Document
+{
+    public int $pageSize;
+
+    /**
+     * @param DocumentCollection<T> $items
+     */
+    public function __construct(
+        public DocumentCollection $items,
+        public int $page,
+        public int $totalPages,
+        public int $total,
+    ) {
+        $this->pageSize = $items->count();
+    }
+
+    public function toArray(?ContextService $context = null): array
+    {
+        return [
+            'pageSize' => $this->pageSize,
+            'items' => $this->items->toArray($context)['items'],
+            'page' => $this->page,
+            'total' => $this->total,
+            'totalPages' => $this->totalPages,
+        ];
+    }
+}
