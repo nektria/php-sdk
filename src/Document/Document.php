7dab2c185d946a334c4a29aea1f24ba9e7640225
xgc <git@xgc.es>
2026-05-25 11:37:54 +0200

    auto commit

--- src/Document/Document.php
+++ src/Document/Document.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 namespace Nektria\Document;
 
+use LogicException;
 use Nektria\Service\ContextService;
+use function sprintf;
 
 readonly abstract class Document
 {
+    public function __toString(): string
+    {
+        if (property_exists($this, 'id')) {
+            return (string) $this->id;
+        }
+
+        throw new LogicException(sprintf('Document %s does not have an id attribute', $this::class));
+    }
+
     /**
      * @return mixed[]
      */
