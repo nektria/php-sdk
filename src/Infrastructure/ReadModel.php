5166cbbe74976d1a1133ead976983ee377d39842
xgc <git@xgc.es>
2026-05-27 21:43:55 +0200

    increase version

--- src/Infrastructure/ReadModel.php
+++ src/Infrastructure/ReadModel.php
@@ -12,6 +12,7 @@ use Nektria\Document\PaginatedDocumentCollection;
 use Nektria\Exception\NektriaException;
 use Nektria\Util\StringUtil;
 use Throwable;
+
 use function count;
 use function is_array;
 
